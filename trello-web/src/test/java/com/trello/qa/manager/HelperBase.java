8d16a3fff54f18b02ef8dfc4b62bd074ca3f184c
Eliezer Freidkin <54038374+lfreidkin@users.noreply.github.com>
2019-10-03 20:21:28 +0300

    new

--- trello-web/src/test/java/com/trello/qa/manager/HelperBase.java
+++ trello-web/src/test/java/com/trello/qa/manager/HelperBase.java
@@ -48,7 +48,6 @@ public class HelperBase {
     } else
       waitForElementAndClick(By.cssSelector("a[href='/']"), 15);
   }
-
   @BeforeMethod
   public void isOnHomePage() {
     if (!isTherePersonalBoards()) {
--- trello-web/src/test/java/com/trello/qa/manager/TestBase.java
+++ trello-web/src/test/java/com/trello/qa/manager/TestBase.java
@@ -10,6 +10,12 @@ public class TestBase {
 
 
   protected static ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
+  @BeforeMethod
+  public void isOnHomePage() {
+    if (!app.boardHelper.isTherePersonalBoards()) {
+      app.boardHelper.returnToHomePage();
+    }
+  }
 
   @BeforeSuite
   public void setUp() {
